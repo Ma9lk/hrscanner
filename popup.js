250eb549d4edacfde6da4b761a66a938ae3cd0fd
Ma9lk <mayk1909@gmail.com>
2020-05-20 01:10:41 +0300

    add one more script

--- popup.js
+++ popup.js
@@ -1,16 +1,26 @@
-const btns = document.getElementsByClassName("btn")
-for (const btn of btns) {
-	btn.addEventListener('click', function () {
-		btn.classList.toggle('active')
-	})
-}
-
-const pencil = document.getElementById("img")
-	pencil.addEventListener('click', function () {
-		document.getElementById("infoCandidate").disabled = false
+const pencil = querySelector('img')
+	pencil.addEventListener('click', function() {
+		document.getElementById('comment')
 	})
 
-const reload = document.getElementById("reboot")
-	reload.addEventListener('click', function () {
-		window.location.reload()
-	})
+// const btns = document.getElementsByClassName("btn")
+// for (const btn of btns) {
+// 	btn.addEventListener('click', function () {
+// 		btn.classList.toggle('active')
+// 	})
+// }
+
+// const reload = document.getElementById("reboot")
+// 	reload.addEventListener('click', function () {
+// 		window.location.reload()
+// 	})
+
+// 	const select = document.querySelector('#position');
+// 	const button = document.querySelector('#conference');
+// 	select.addEventListener('change', function() {
+// 		if (this.value === 'interview') {
+// 			button.style.display = 'block';
+// 		} else {
+// 			button.style.display = 'none';
+// 		}
+// 	})
